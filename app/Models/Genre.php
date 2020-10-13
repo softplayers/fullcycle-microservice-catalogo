4a55263cc4e020c4dda59acf90b1e85a3c12356a
Renato <accounts01@softplayers.com>
2020-10-13 20:52:27 -0300

    feat: add genres endpoit

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Category extends Model
+class Genre extends Model
 {
     use SoftDeletes, \App\Models\Traits\Uuid;
-    protected $fillable = ['name', 'description', 'is_active'];
+    protected $fillable = ['name', 'is_active'];
     protected $dates = ['deleted_at'];
     protected $casts = ['id' => 'string'];
 }
