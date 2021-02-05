b96526ff906e47e94fa09787b73f0140eebebbc8
Renato <accounts01@softplayers.com>
2021-02-05 14:09:29 -0300

    styles

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react';
-import {AppBar, Toolbar} from '@material-ui/core';
+import {AppBar, Toolbar, Button, Typography, makeStyles} from '@material-ui/core';
+import logo from '../../static/img/logo.png';
+
+const useStyles = makeStyles({
+  toolbar: {
+    backgroundColor: '#000000'
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'center'
+  }
+});
 
 export const Navbar: React.FC = () => {
+  const classes = useStyles();
+
   return (
     <AppBar>
-      <Toolbar>Teste</Toolbar>
+      <Toolbar className={classes.toolbar}>
+          <Typography className={classes.title}>
+            <img src={logo} alt="CodeFlix"/>
+          </Typography>
+          <Button color="inherit">Login</Button>
+      </Toolbar>
     </AppBar>
   );
 };
