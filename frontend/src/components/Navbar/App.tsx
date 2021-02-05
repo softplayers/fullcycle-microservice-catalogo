6f76210982a0a39006769d0cb1e8ee23bccf6f56
Renato <accounts01@softplayers.com>
2021-02-05 16:41:25 +0000

    chore: update docker compose

---
+++ frontend/src/components/Navbar/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@material-ui/core';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button color="primary" variant="outlined">Texto</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
