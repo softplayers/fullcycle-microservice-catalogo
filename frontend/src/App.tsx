4cfd180a97d088886b3a5541bc13c9ed4c28de75
Renato <accounts01@softplayers.com>
2021-02-12 14:04:20 -0300

    feat: migalhas de pãos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { Navbar } from './components/Navbar';
 import AppRouter from './routes/AppRouter';
+import Breadcrumbs from './components/Breadcrumbs';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Navbar></Navbar>
         <Box paddingTop={'70px'}>
+          <Breadcrumbs/>
           <AppRouter/>
         </Box>
       </BrowserRouter>
