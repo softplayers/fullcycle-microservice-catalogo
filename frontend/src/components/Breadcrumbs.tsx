f23a27ec74af241f7dbd5447bcfad1de0c6ed6f0
Renato <accounts01@softplayers.com>
2021-02-15 14:05:11 -0300

    more bread crumbs

--- frontend/src/components/Breadcrumbs.tsx
+++ frontend/src/components/Breadcrumbs.tsx
@@ -1,45 +1,16 @@
 /* eslint-disable no-nested-ternary */
 import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
 import Link, { LinkProps } from '@material-ui/core/Link';
-import ListItem from '@material-ui/core/ListItem';
-import Collapse from '@material-ui/core/Collapse';
-import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
 import MuiBreadcrumbs from '@material-ui/core/Breadcrumbs';
-import { Route, MemoryRouter } from 'react-router';
+import { Route } from 'react-router';
 import { Link as RouterLink } from 'react-router-dom';
-import { Omit } from '@material-ui/types';
+import {Location} from 'history';
+import routes from '../routes';
 
-interface ListItemLinkProps extends LinkProps {
-  to: string;
-  open?: boolean;
-}
-
-const breadcrumbNameMap: { [key: string]: string } = {
-  '/inbox': 'Inbox',
-  '/inbox/important': 'Important',
-  '/trash': 'Trash',
-  '/spam': 'Spam',
-  '/drafts': 'Drafts',
-};
-
-function ListItemLink(props: Omit<ListItemLinkProps, 'ref'>) {
-  const { to, open, ...other } = props;
-  const primary = breadcrumbNameMap[to];
-
-  return (
-    <li>
-      <ListItem button component={RouterLink} to={to} {...other}>
-        <ListItemText primary={primary} />
-        {open != null ? open ? <ExpandLess /> : <ExpandMore /> : null}
-      </ListItem>
-    </li>
-  );
-}
+const breadcrumbNameMap: { [key: string]: string } = {};
+routes.forEach(route => breadcrumbNameMap[route.path as string] = route.label);
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -67,55 +38,41 @@ const LinkRouter = (props: LinkRouterProps) => <Link {...props} component={Route
 
 export default function Breadcrumbs() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
 
-  const handleClick = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
+  function makeBreadcrumbs(location: Location) {
+    const pathnames = location.pathname.split('/').filter(x => x);
+    pathnames.unshift('/');
+
+    return (
+      <MuiBreadcrumbs aria-label="breadcrumb">
+        {
+          pathnames.map((value, index) => {
+            const last = index === pathnames.length - 1;
+            const to = `/${pathnames.slice(0, index + 1).join('/')}`;
+
+            return last ? (
+              <Typography color="textPrimary" key={to}>
+                {breadcrumbNameMap[to]}
+              </Typography>
+            ) : (
+              <LinkRouter color="inherit" to={to} key={to}>
+                {breadcrumbNameMap[to]}
+              </LinkRouter>
+            );
+          })
+        }
+      </MuiBreadcrumbs>
+    );
+  }
 
   return (
-    <MemoryRouter initialEntries={['/inbox']} initialIndex={0}>
-      <div className={classes.root}>
-        <Route>
-          {({ location }) => {
-            const pathnames = location.pathname.split('/').filter((x) => x);
-
-            return (
-              <MuiBreadcrumbs aria-label="breadcrumb">
-                <LinkRouter color="inherit" to="/">
-                  Home
-                </LinkRouter>
-                {pathnames.map((value, index) => {
-                  const last = index === pathnames.length - 1;
-                  const to = `/${pathnames.slice(0, index + 1).join('/')}`;
-
-                  return last ? (
-                    <Typography color="textPrimary" key={to}>
-                      {breadcrumbNameMap[to]}
-                    </Typography>
-                  ) : (
-                    <LinkRouter color="inherit" to={to} key={to}>
-                      {breadcrumbNameMap[to]}
-                    </LinkRouter>
-                  );
-                })}
-              </MuiBreadcrumbs>
-            );
-          }}
-        </Route>
-        <nav className={classes.lists} aria-label="mailbox folders">
-          <List>
-            <ListItemLink to="/inbox" open={open} onClick={handleClick} />
-            <Collapse component="li" in={open} timeout="auto" unmountOnExit>
-              <List disablePadding>
-                <ListItemLink to="/inbox/important" className={classes.nested} />
-              </List>
-            </Collapse>
-            <ListItemLink to="/trash" />
-            <ListItemLink to="/spam" />
-          </List>
-        </nav>
-      </div>
-    </MemoryRouter>
+    <div className={classes.root}>
+      <Route>
+        {
+          ({location}: {location: Location}) => makeBreadcrumbs(location)
+        }
+      </Route>
+    </div>
   );
+
 }
