62c6dd200306fba21d2a0b62095db40061aef738
Renato <accounts01@softplayers.com>
2021-02-17 13:32:29 -0300

    mui tabeles

--- frontend/src/pages/category/Table.tsx
+++ frontend/src/pages/category/Table.tsx
@@ -1,15 +1,40 @@
 // @flow 
+import MUIDataTable, {MUIDataTableColumn} from 'mui-datatables';
 import * as React from 'react';
 
-type Props = {
-    
-};
+const columnsDefinition: MUIDataTableColumn[] = [
+    {
+        name: "name",
+        label: "Nome",
+    },
+    {
+        name: "is_active",
+        label: "Ativo?",
+    },
+    {
+        name: "created_at",
+        label: "Criado em",
+    },
+]
+
+const data = [
+    {name: 'teste1', is_active: true, created_at: "2021-02-17"},
+    {name: 'teste2', is_active: true, created_at: "2021-02-18"},
+    {name: 'teste3', is_active: true, created_at: "2021-02-19"},
+    {name: 'teste4', is_active: true, created_at: "2021-02-20"},
+    {name: 'teste5', is_active: true, created_at: "2021-02-21"},
+    {name: 'teste6', is_active: true, created_at: "2021-02-22"},
+]
+
+type Props = {};
 
 const Table = (props: Props) => {
     return (
-        <div>
-            table
-        </div>
+        <MUIDataTable 
+            title="Listagem de categorias"
+            columns={columnsDefinition}
+            data={data}>
+        </MUIDataTable>
     );
 };
 
