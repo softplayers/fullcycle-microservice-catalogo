c89efd0e3d54e4e82fe16ae6a3b2d9411f7912b5
Renato <accounts01@softplayers.com>
2021-03-18 21:59:13 +0000

    feat: form cast member edit

--- frontend/src/pages/cast_member/Form.tsx
+++ frontend/src/pages/cast_member/Form.tsx
@@ -1,14 +1,17 @@
 // @flow 
-import * as React from 'react';
-import { useForm } from "react-hook-form";
+import { Box, Button, ButtonProps, makeStyles, TextField, Theme } from '@material-ui/core';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormLabel from '@material-ui/core/FormLabel';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-
-import { Box, Button, ButtonProps, Checkbox, makeStyles, TextField, Theme } from '@material-ui/core';
+import { useSnackbar } from 'notistack';
+import * as React from 'react';
+import { useForm } from "react-hook-form";
+import { useParams, useHistory } from 'react-router';
 import castMemberHttp from '../../util/http/cast-member-http';
+import * as yup from '../../util/vendor/yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 const useStyles = makeStyles((theme: Theme) => {
     return {
@@ -18,33 +21,96 @@ const useStyles = makeStyles((theme: Theme) => {
     }
 });
 
+const validationSchema = yup.object().shape({
+    name: yup
+        .string()
+        .label('Nome')
+        .required()
+})
+
+interface IFormInputs {
+    name: string
+    type: number
+}
+
+
 export const Form = () => {
     const classes = useStyles();
+    const history = useHistory();
+    const snackbar = useSnackbar();
 
-    const [value, setValue] = React.useState('1');
+    //const [value, setValue] = React.useState('1');   
+
+    const { register, handleSubmit, getValues, setValue, errors, reset, watch } = useForm<IFormInputs>({
+        resolver: yupResolver(validationSchema),
+        defaultValues: {
+            type: 1
+        }
+    });
+
+    const { id } = useParams<any>();
+    const [castMember, setCastMember] = React.useState<{ id: string } | null>(null);
+    const [loading, setLoading] = React.useState<boolean>(false);
 
     const buttonProps: ButtonProps = {
         className: classes.submit,
         color: 'secondary',
         variant: "contained",
+        disabled: loading
     }
 
-    const { register, handleSubmit, getValues } = useForm({
-        defaultValues: {
-            is_active: true,
-        }
-    });
+    React.useEffect(() => {
+        if (!id) return;
+        setLoading(true);
 
+        castMemberHttp
+            .get(id)
+            .then(({ data }) => {
+                setCastMember(data.data);
+                reset(data.data);
+            })
+            .finally(() => setLoading(false));
+    }, []);
+
+    React.useEffect(() => {
+        register({ name: 'type' });
+    }, [register]);
+
+    const handleRadio = (event) => {
+        console.log(event);
+        setValue('type', parseInt(event.target.value));
+    };
     const onSubmit = (data, event) => {
         console.log(data, event);
-        castMemberHttp
-            .create({...data, type: value})
-            .then(response => console.log(response));
-    }
+        setLoading(true);
 
-    const handleChange = (event) => {
-        setValue(event.target.value);
-    };
+        const http = castMember
+            ? castMemberHttp.update(castMember.id, data)
+            : castMemberHttp.create(data);
+
+        http
+            .then(({ data }) => {
+                const hasEvent = !!event;
+                snackbar.enqueueSnackbar('Membro de elenco salvo com sucesso!', { variant: 'success' });
+
+                if (!hasEvent) {
+                    history.push(`/cast_members`);
+                    return;
+                }
+
+                if (id) {
+                    history.replace(`/cast_members/${data.data.id}/edit`)
+                    return;
+                }
+
+                history.push(`/cast_members/${data.data.id}/edit`);
+            })
+            .catch(error => {
+                console.error(error);
+                snackbar.enqueueSnackbar('Erro ao salvar!', { variant: 'error' });
+            })
+            .finally(() => setLoading(!false));
+    }
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -54,15 +120,30 @@ export const Form = () => {
                 fullWidth
                 variant={"outlined"}
                 inputRef={register}
+                disabled={loading}
+                error={!!errors.name}
+                helperText={errors.name?.message}
+                InputLabelProps={{ shrink: true }}
             />
+            {
+                errors.name && errors.name.type === 'required' &&
+                (<p>{errors.name?.message}</p>)
+            }
 
-            <FormControl component="fieldset">
-                <FormLabel component="legend">Tipo</FormLabel>
-                <RadioGroup aria-label="type" name="type" value={value} onChange={handleChange}>
-                    <FormControlLabel value="1" control={<Radio />} label="Diretor" />
-                    <FormControlLabel value="2" control={<Radio />} label="Ator" />
-                </RadioGroup>
-            </FormControl>
+            <FormControlLabel
+                disabled={loading}
+                control={
+                    <RadioGroup
+                        name="type"
+                        value={watch('type')}
+                        onChange={handleRadio}>
+                        <FormControlLabel value={1} control={<Radio />} label="Diretor"  disabled={loading} />
+                        <FormControlLabel value={2} control={<Radio />} label="Ator"  disabled={loading}/>
+                    </RadioGroup>
+                }
+                label="Tipo"
+                labelPlacement="top"
+            />
 
             <Box dir={'rtl'}>
                 <Button {...buttonProps} type="button" onClick={() => onSubmit(getValues(), null)} color="primary">Salvar</Button>
